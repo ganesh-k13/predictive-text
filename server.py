fa9c3869815e887ae8633ef32d987eba7dd752e1
Ganesh K <ganesh3597@gmail.com>
2018-11-18 18:11:00 +0530

    Server Init

--- server.py
+++ server.py
@@ -1,6 +1,8 @@
 from flask import Flask, request, render_template
 import subprocess
 import os
+from markov import MarkovChain
+import random
 
 app = Flask(__name__)
 
@@ -10,9 +12,19 @@ def index():
 
 @app.route('/handle_data',methods = ['POST'])
 def handle_data():
-	inputText = request.form['user_text']
-	os.system('python3 markov.py dataset/big/w2_.txt 2');
-	return inputText + '\n'
+	
+	word1 = request.form["1"]
+	word2 = request.form["2"]
+	
+	m = MarkovChain("Database/w3_.db", n = 3)
+	# pprint(m.query(*(options.predict[0])))
+	ngram = [word1, word2]
+	res = m.query(*ngram).keys() # Actual Result
+	print(res)
+	# word = list(res)[random.randint(0, 2)]
+	# print(word, end = ' ')
+
+	return str(dict(zip(["predict1", "predict2", "predict3"], list(res)[:3])))
     
 if(__name__ == '__main__'):
     app.run(debug=True)
